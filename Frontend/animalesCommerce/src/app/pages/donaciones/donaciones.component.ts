37ca3e85e14eb829de59652cc25b92539a157d98
Nico Mattos <106168031+NicolasMattos03@users.noreply.github.com>
2024-05-06 01:08:00 -0300

    Update front Donaciones y Veterinarios

--- Frontend/animalesCommerce/src/app/pages/donaciones/donaciones.component.ts
+++ Frontend/animalesCommerce/src/app/pages/donaciones/donaciones.component.ts
@@ -1,29 +1,28 @@
-
 import { Component, OnInit } from '@angular/core';
 import { DonacionesService } from 'src/app/services/donaciones.service';
 
 @Component({
   selector: 'app-donaciones',
   templateUrl: './donaciones.component.html',
-  styleUrls: ['./donaciones.component.css']
+  styleUrls: ['./donaciones.component.css'],
 })
 export class DonacionesComponent implements OnInit {
+  lista: any;
 
-  datos:any;
- constructor (private listPersonas: DonacionesService){}
+  constructor(private listDona: DonacionesService) {}
 
-ngOnInit():void{
-  this.verDonaciones();
-}
+  ngOnInit(): void {
+    this.listaDonaciones();
+  }
 
-verDonaciones():void{
-  this.listPersonas.getDonaciones().subscribe({
-    next:(response)=>(
-      this.datos=response
-      ),
-    error: (errorResponse)=> (
-      console.error(errorResponse)
-    )
-  })
- }
+  listaDonaciones(): void {
+    this.listDona.verDonaciones().subscribe({
+      next: (response) => {
+        this.lista = response;
+      },
+      error: (errorResponse) => {
+        console.error(errorResponse);
+      },
+    });
+  }
 }
